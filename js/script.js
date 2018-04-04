fe05ef48c342081be1f735ef96cba61eaa3d1719
Piotr Pidlypczak <peterpid@gmail.com>
2018-04-04 20:39:39 +0200

    Add more data: field, capital, flag

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function(){
 	var getCountryNameUrl = 'https://restcountries.eu/rest/v2/name/';
 	var countryName = '';
-	var countriesList = $('#countries');
+	var $countriesList = $('#countries-list');
 
 	$('#search').click(searchCountries);
 
@@ -12,7 +12,7 @@ $(function(){
 		}
 
 		$.ajax({
-			url: getCountryNameUrl + countryName,
+			url: getCountryNameUrl + countryName + '?fields=name;capital;currencies;flag',
 			method: 'GET',
 			success: showCountriesList,
 			error: showNoData
@@ -20,17 +20,60 @@ $(function(){
 	}
 
 	function showCountriesList(response) {
-		countriesList.empty();
+		$countriesList.empty();
 		var matchesCount = 0;
 		response.forEach(function(item) {
-			$('<li>').text(item.name).appendTo(countriesList);
+			
+			// why this section does not work?
+			/*
+			var $listItem = $('<li>').append(new Country(item));
+			//$countriesList.append($listItem);
+			$listItem.appendTo($countriesList);
+			*/
+			 
+			// why only this section works?
+			var $listItem = $('<li>').text(item.name);
+			$listItem.appendTo($countriesList);
+			
 		});
 		$('#search-status').text('Found ' + response.length + ' matching items for "' + countryName + '"');
 
 	}
 
 	function showNoData() {
-		countriesList.empty();
+		$countriesList.empty();
 		$('#search-status').text('No items found for: ' + '"' + countryName + '"');
 	}
+
+	function Country(responseItem) {
+		var self = this;
+		this.name = responseItem.name;
+		this.flag = responseItem.flag;
+		this.capital = responseItem.capital;
+		this.currency = responseItem.currencies.name;
+
+		this.$elemant = createCountry();
+
+		function createCountry() {
+			var $country = $('<div>').addClass('country-item');
+			console.log('adding: ' + self.name);
+			var $countryName = $('<div>').addClass('country-name').text(self.name);
+			var $flag = $('<img>').addClass('flag').attr('src', self.flag);
+			var $capital = $('<div>').addClass('capital').text(self.capital);
+			var $currency = $('<div>').addClass('currency').text(self.currency);
+
+			$country.append($countryName)
+				.append($flag)
+				.append($capital)
+				.append($currency);
+			//console.log('created: '+ $country.text());
+			return $country;
+		}
+	}
+
+	//Country.prototype = {
+	// 	// what shoud go here?
+	//};
+
+
 });
